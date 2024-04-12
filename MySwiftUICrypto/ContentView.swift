29e2f2e7a5d910b8d40c266260be5fb80b9e09eb
erwinroblesjr <68773098+erwinroblesjr@users.noreply.github.com>
2024-04-11 20:10:34 -0400

    Initial Commit

---
+++ MySwiftUICrypto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MySwiftUICrypto
+//
+//  Created by Erwin Robles on 4/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MySwiftUICrypto/MySwiftUICryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftUICryptoApp.swift
+//  MySwiftUICrypto
+//
+//  Created by Erwin Robles on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftUICryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
