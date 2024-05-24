557f422ce751bad079aa341476f57a0e1a925702
erwinroblesjr <68773098+erwinroblesjr@users.noreply.github.com>
2024-05-24 00:59:53 -0400

    Added 'extensions' folder Added 'Color' extension file Added 'ColorTheme' function

---
+++ MySwiftUICrypto/Extensions/Colors.swift
@@ -0,0 +1,20 @@
+//
+//  Colors.swift
+//  MySwiftUICrypto
+//
+//  Created by Erwin Robles on 5/24/24.
+//
+
+import SwiftUI
+
+extension Color{
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme{
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let secondaryText = Color("SecondaryTextColor")
+    let greenText = Color("GreenTextColor")
+    let redText = Color("RedTextColor")
+}
